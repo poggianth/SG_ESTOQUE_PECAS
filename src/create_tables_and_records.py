3e112522dd8c5f177e6143004f7f35e212b3f5c3
Thiago Melo Poggian <poggianth@gmail.com>
2023-10-17 21:43:11 -0300

    remove trash

--- src/create_tables_and_records.py
+++ src/create_tables_and_records.py
@@ -55,30 +55,7 @@ def run():
         ALTER TABLE item_estoque ADD CONSTRAINT item_estoque_id_produto_produto_id FOREIGN KEY (id_produto) REFERENCES produto(id);
     """
 
-    query_insert_records = """
-        INSERT INTO estoque (tipo) VALUES ('Estoque Principal');
-        INSERT INTO estoque (tipo) VALUES ('Estoque de Backup');
-        INSERT INTO estoque (tipo) VALUES ('Estoque de Excesso');
-
-        INSERT INTO produto (nome, descricao, quantidade, categoria, preco_unitario, quantidade_reposicao) VALUES ('Produto 1', 'Descrição do Produto 1', 100, 'Categoria A', 10.99, 50);
-        INSERT INTO produto (nome, descricao, quantidade, categoria, preco_unitario, quantidade_reposicao) VALUES ('Produto 2', 'Descrição do Produto 2', 200, 'Categoria B', 15.50, 30);
-        INSERT INTO produto (nome, descricao, quantidade, categoria, preco_unitario, quantidade_reposicao) VALUES ('Produto 3', 'Descrição do Produto 3', 150, 'Categoria A', 8.75, 40);
-    """
-
-    query_insert_related_records = """
-        INSERT INTO item_estoque (id_estoque, id_produto, estante, prateleira) VALUES (1, 1, 'Estante A', 1);
-        INSERT INTO item_estoque (id_estoque, id_produto, estante, prateleira) VALUES (1, 2, 'Estante B', 2);
-        INSERT INTO item_estoque (id_estoque, id_produto, estante, prateleira) VALUES (2, 2, 'Estante A', 3);
-        INSERT INTO item_estoque (id_estoque, id_produto, estante, prateleira) VALUES (3, 3, 'Estante C', 4);
-    """
-
     crate_tables(query_create_tables)
     print("Tables successfully created!")
-
-    generate_records(query_insert_records)
-    print("Records successfully generated!")
-
-    generate_records(query_insert_related_records)
-    print("Records successfully generated!")
 
 run()
